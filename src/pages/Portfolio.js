eacacb01a329e76f3c43b7c2d480392283a46ebe
Pete Low <pedritolow@gmail.com>
2020-12-03 18:37:04 -0800

    imported images to porfolio and put them in an array

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,6 +5,14 @@ import ProjectCard from '../components/Project Card';
 import portfolioContent from '../portfolioContent.json';
 import Col from '../components/Col';
 import Row from '../components/Row';
+import sk8tool from '../assets/sk8tool.png'
+import burgerApp from '../assets/burgerApp.png'
+import skatefinder from '../assets/skatefinder.png'
+import weatherdashboard from '../assets/weatherdashboard.png'
+import scheduler from '../assets/scheduler.png'
+import codeQuiz from '../assets/codeQuiz.png'
+
+const portfolioImages = [sk8tool, burgerApp, skatefinder, weatherdashboard, scheduler, codeQuiz]
 
 
 class Portfolio extends Component {
@@ -18,12 +26,12 @@ class Portfolio extends Component {
             <Container>
                 <Row>
                     <Col size='md-6'>
-                        {portfolioContent.map(portfolioItem => (
+                        {this.state.portfolioContent.map((portfolioItem, i) => (
                         <ProjectCard
                         id={portfolioItem.id}
                         key={portfolioItem.id}
                         title={portfolioItem.title}
-                        image={portfolioItem.image}
+                        image={portfolioImages[i]}
                         description={portfolioItem.description}
                         link={portfolioItem.link} 
                         />
